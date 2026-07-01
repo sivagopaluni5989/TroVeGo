fcc33cb84383b6067098f6199fc7c11f3b4fa78f
sivagopaluni5989 <your-email@example.com>
2026-07-01 05:52:23 +0000

    Add important Flutter project files and release assets

--- android/app/src/main/kotlin/com/shivagsudhir/store_gui_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/shivagsudhir/store_gui_app/MainActivity.kt
@@ -1,5 +1,48 @@
 package com.shivagsudhir.store_gui_app
 
+import android.media.MediaScannerConnection
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    private val CHANNEL = "media_scanner"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+
+            if (call.method == "scanFile") {
+
+                val path = call.argument<String>("path")
+
+                if (path != null) {
+
+                    MediaScannerConnection.scanFile(
+                        this,
+                        arrayOf(path),
+                        null,
+                        null
+                    )
+
+                    result.success(true)
+
+                } else {
+                    result.error(
+                        "NO_PATH",
+                        "File path missing",
+                        null
+                    )
+                }
+
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
